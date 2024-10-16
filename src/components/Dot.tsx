4f11c5e3412d66f6f145ad2a8aa707c82f693986
Shravan <shravansinghchaudhary@gmail.com>
2024-10-16 22:38:48 +0530

    refactor: Update Dot component to display the length of cartItems in the cart

--- src/components/Dot.tsx
+++ src/components/Dot.tsx
@@ -2,7 +2,7 @@ import { useAppSelector } from "@/lib/store/hooks";
 import React from "react";
 
 const Dot = () => {
-    const value = useAppSelector((state) => state.cart.value);
+    const value = useAppSelector((state) => state.cart.cartItems.length);
     return (
         <span className="absolute -right-1 -top-0 flex size-[4px] items-center justify-center rounded-full bg-primary p-[9px] font-bold text-white">
             {value}
