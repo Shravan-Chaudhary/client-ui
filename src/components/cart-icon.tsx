1c79c6b800036090e0edbf78a468509cf2f3b51a
Shravan <shravansinghchaudhary@gmail.com>
2024-10-17 13:59:46 +0530

    feat: :ambulance: Fix hydration error on dot component

--- src/components/cart-icon.tsx
+++ src/components/cart-icon.tsx
@@ -3,15 +3,17 @@ import React from "react";
 import { buttonVariants } from "./ui/button";
 import basket from "@/images/basket.png";
 import Image from "next/image";
-import Dot from "./dot";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import dynamic from "next/dynamic";
+
+const DotWithoutSSR = dynamic(() => import("./dot"), { ssr: false });
 
 const CartIcon = () => {
     return (
         <Link href={"/cart"} className={cn(buttonVariants({ variant: "ghost", size: "sm" }), "relative mr-5")}>
             <Image src={basket} alt="basket" width={23} height={23} />
-            <Dot />
+            <DotWithoutSSR />
         </Link>
     );
 };
