bba3df807cfb107cff5eaa2f581cbf96600ad7db
Shravan <shravansinghchaudhary@gmail.com>
2024-10-21 10:24:51 +0530

    feat: Add MobileCartItem and CheckoutButton components to CartList

--- src/app/cart/_components/cart-list.tsx
+++ src/app/cart/_components/cart-list.tsx
@@ -2,6 +2,9 @@ import { Card } from "@/components/ui/card";
 import React from "react";
 import CartItem from "./cart-item";
 import { Separator } from "@/components/ui/separator";
+import MobileCartItem from "./mobile-cart-item";
+import CheckoutButton from "./checkout-button";
+import Link from "next/link";
 
 const CartList = () => {
     return (
@@ -10,7 +13,15 @@ const CartList = () => {
             {/* Divider */}
             <CartItem />
             <Separator className="my-4" />
+            <MobileCartItem />
+            <Separator className="my-4" />
             {/* Price and Checkout */}
+            <div className="mt-6 flex items-center justify-between">
+                <p className="text-base font-semibold md:text-lg">&#8377;2000</p>
+                <Link href={"/checkout"}>
+                    <CheckoutButton />
+                </Link>
+            </div>
         </Card>
     );
 };
