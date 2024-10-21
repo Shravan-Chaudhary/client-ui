7e4819d4a2af7bc036aa6a7553e41a0d721a3a4b
Shravan <shravansinghchaudhary@gmail.com>
2024-10-21 10:25:27 +0530

    feat: Create CheckoutPage component

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,8 @@
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import React from "react";
+
+const CheckoutPage = () => {
+    return <MaxWidthWrapper className="mb-12 mt-10">CheckoutPage</MaxWidthWrapper>;
+};
+
+export default CheckoutPage;
