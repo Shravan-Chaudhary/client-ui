beae3e5bf5d9b8603fb4195399d91248558ef5f4
Shravan <shravansinghchaudhary@gmail.com>
2024-12-19 15:23:01 +0530

    feat: :necktie: Redirect unauthenticated users to login page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,8 +2,16 @@ import MaxWidthWrapper from "@/components/max-width-wrapper";
 import React from "react";
 import CustomerDetailsCard from "./_components/customer-details-card";
 import OrderSummaryCard from "./_components/order-summary-card";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
-const CheckoutPage = () => {
+const CheckoutPage = async () => {
+    const session = await getSession();
+
+    if (!session) {
+        redirect("/sign-in");
+    }
+
     return (
         <MaxWidthWrapper className="mb-12 mt-10">
             <div className="grid size-full grid-cols-1 gap-6 lg:grid-cols-12">
