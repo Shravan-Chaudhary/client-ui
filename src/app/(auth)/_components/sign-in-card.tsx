13cefec0bffc359fa617b0ae8f3a269223727c1d
Shravan <shravansinghchaudhary@gmail.com>
2025-04-24 15:35:55 +0530

    style: :art: Update Sign In Card links for consistent casing and improved readability

--- src/app/(auth)/_components/sign-in-card.tsx
+++ src/app/(auth)/_components/sign-in-card.tsx
@@ -101,7 +101,7 @@ const SigninCard = () => {
                     className="relative text-sm text-primary transition-all duration-300 ease-in-out after:absolute after:bottom-0 after:left-0 after:h-px after:w-full after:origin-bottom-right after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 after:ease-in-out hover:after:origin-bottom-left hover:after:scale-x-100"
                     href="/sign-up"
                 >
-                    Sign up
+                    Sign Up
                 </Link>
             </CardFooter>
         </Card>
